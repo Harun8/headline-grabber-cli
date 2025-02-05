307f5e4a90bdb14bb94d96bb413146d1134cb6b6
Harun8 <harunabdi8@gmail.com>
2025-02-05 10:47:19 +0100

    inital commit

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ module.exports = () => {
       break
 
     default:
-      error(`"${cmd}" is not a valid command`, true)
+      error(`"${cmd}" is not a valid command..`, true)
       break
   }
 }
